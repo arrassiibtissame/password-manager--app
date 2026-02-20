6ae261a55ea53ccee7d6e8c533a07824d7393c0c
arrassiibtissame <ibtissame2.ar@gmail.com>
2026-02-20 20:37:41 +0200

    add database connection and udating the server file

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
 
 const app = express();
 
+// connect database
+connectDB();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send("Password Manager API is running");
+    res.send("Password Manager API running");
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
