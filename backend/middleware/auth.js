56dfa22d4d3952fcc2e535e7b9f49db5583e3451
arrassiibtissame <ibtissame2.ar@gmail.com>
2026-04-12 19:33:20 +0300

    fix api erros

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,13 +1,19 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next){
+    console.log("HEADERS:", req.headers);
     const authHeader = req.headers["Authorization"];
     if (!authHeader){
         return res.status (401).json({message:"No token,access denied"});
 
     }
+    // Split "Bearer TOKEN"
+    const token = authHeader.split (" ")[1];
+    if (!token){
+        return res.status (401).json({message:"No token,access denied"});
+
+    }
     try {
-        const token = authHeader.replace("Bearer","");
         const decoded = jwt.verify(token,process.env.JWT_SECRET);
         req.user = decoded;
         next();}
