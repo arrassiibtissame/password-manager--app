3365db6543bf996c9d1eb407f3670de0ac01e9c2
arrassiibtissame <ibtissame2.ar@gmail.com>
2026-04-12 19:40:48 +0300

    update auth

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,18 +1,19 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next){
-    console.log("HEADERS:", req.headers);
-    const authHeader = req.headers["Authorization"];
+    console.log("HEADERS:", req.headers.authorization);
+    const authHeader = req.headers.authorization;
     if (!authHeader){
-        return res.status (401).json({message:"No token,access denied"});
+        return res.status(401).json({message:"No token,access denied"});
 
     }
     // Split "Bearer TOKEN"
-    const token = authHeader.split (" ")[1];
-    if (!token){
-        return res.status (401).json({message:"No token,access denied"});
-
+    const parts = authHeader.split(" ");
+    if (parts.length !== 2 || parts[0] !== "Bearer"){
+        return res.status(401).json({message:"Token format invalid"});
     }
+    const token = parts[1];
+    
     try {
         const decoded = jwt.verify(token,process.env.JWT_SECRET);
         req.user = decoded;
