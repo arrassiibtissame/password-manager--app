a134d29c99c38e4419b6911476a2845f0cb61a4c
arrassiibtissame <ibtissame2.ar@gmail.com>
2026-04-16 01:45:48 +0300

    fix errors

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,13 +5,22 @@ function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const { login, error } = useAuth();
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
-    const handleLogin = async () => {
-        try {
-            await login(email, password);
-            navigate ("/dashboard");
-        } catch {}
-    };
+   const handleLogin = async () => {
+  setLoading(true); // start loading
+
+  try {
+    const res = await api.post("/auth/login", { email, password });
+
+    login(res.data.token);
+    navigate("/dashboard");
+  } catch (err) {
+    alert(err.response?.data?.message || err.message);
+  } finally {
+    setLoading(false); // stop loading
+  }
+};
     return (
       <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-900 to-black text-white">
         <div className= "w-[350px] p-6 rounded-2xl bg-white/10 backdrop-blur border border-white/20">
@@ -30,11 +39,12 @@ function Login() {
         onChange={(e) => setPassword(e.target.value)}
       />
 {error && (<p className="text-red-500 mb-3">{error}</p>)}
-<button 
-onclick={handleLogin}
-disabled={loading}
-className="w-full bg-blue-600 hover:bg-blue-700 p-2 rounded">
-{loading ? "Logging in..." : "Login"}
+<button
+  onClick={handleLogin}
+  disabled={loading}
+  className="bg-blue-600 p-2 rounded"
+>
+  {loading ? "Logging in..." : "Login"}
 </button>
 <p className="text-sm mt-4 text-center">
   Don't have an account? {" "}
